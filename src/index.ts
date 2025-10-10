7740d6b9c188009e1fec2028053bd0bc29476e5c
aljames-arctic <bakanabaka.vtt@gmail.com>
2025-10-10 17:47:35 -0500

    fix: Quick Action TokenHUD clear/close now called
    TokenHUD clear is being deprecated, Foundry now calls close instead.

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,9 @@ import * as ui from './ui';
 let shownToken: Token | null = null;
 
 Hooks.on('init', () => {
-  const originalClear = foundry.applications.hud.TokenHUD.prototype.clear;
-  foundry.applications.hud.TokenHUD.prototype.clear = function () {
-    originalClear.call(this);
+  const originalClose = foundry.applications.hud.TokenHUD.prototype.close;
+  foundry.applications.hud.TokenHUD.prototype.close = function () {
+    originalClose.call(this);
     shownToken = null;
     ui.hideTokenActions();
   };
